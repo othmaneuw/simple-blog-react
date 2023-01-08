cd725de6f4cd513deba923373581268570b2c102
othmaneuw <strangeothmane@gmail.com>
2023-01-08 19:07:43 +0100

    Loading message

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,21 @@ import Blog from "./Blog";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending,setIsPending] = useState(true);
 
    useEffect(()=>{
     console.log('use effect ran !!');
     fetch("http://localhost:8000/blogs")
        .then(res => res.json())
-       .then(data => setBlogs(data));
+       .then(data => {
+          setBlogs(data);
+          setIsPending(false);
+      });
    },[])
 
 return (
     <div className="home">
+      {isPending && <div>Loading...</div> }
       { blogs && <Blog blogs={blogs} title="All blog!!" /> }
       {/* <Blog blogs={blogs.filter(item => item.author === 'Othmane')} title={"Othmane's Blogs"} /> */}
     </div>
